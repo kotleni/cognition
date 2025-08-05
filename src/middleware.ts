96a7a3222d4fdd41d51183e23752e2de93c90eec
Viktor Varenik <yavarenikya@gmail.com>
2025-08-05 20:57:47 +0300

    chore: integrated auth.js

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import {NextResponse} from 'next/server';
 import type {NextRequest} from 'next/server';
+export {auth as authMiddleware} from '@/auth';
 
 export function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL('/auth', request.url));

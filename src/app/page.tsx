bc30237a8d5d5864d57827fed92cee81488f1055
Viktor Varenik <yavarenikya@gmail.com>
2025-08-13 12:31:29 +0300

    chore: add demo nav menu from shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,40 @@
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from '@/components/ui/navigation-menu';
+
 export default function Home() {
-    return <div className="p-4 text-1xl font-bold">Hello, world!</div>;
+    return (
+        <div className="flex flex-col gap-4 p-4">
+            <section className="w-full">
+                <NavigationMenu>
+                    <NavigationMenuList>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>
+                                Learning
+                            </NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <NavigationMenuLink>Quiz's</NavigationMenuLink>
+                                <NavigationMenuLink>Words</NavigationMenuLink>
+                                <NavigationMenuLink>Cards</NavigationMenuLink>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavigationMenuTrigger>
+                                Account
+                            </NavigationMenuTrigger>
+                            <NavigationMenuContent>
+                                <NavigationMenuLink>Profile</NavigationMenuLink>
+                                <NavigationMenuLink>SignOut</NavigationMenuLink>
+                            </NavigationMenuContent>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </NavigationMenu>
+            </section>
+        </div>
+    );
 }
